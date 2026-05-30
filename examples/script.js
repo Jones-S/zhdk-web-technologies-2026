1eb75b92178b377dbd50c055bd52c978d2a0a067
Jonas <git@jonasscheiwiller.ch>
2026-05-30 22:11:49 +0200

    feat: change examples and add ai made article from screenshot

--- examples/script.js
+++ examples/script.js
@@ -1,8 +1,8 @@
 // This file is linked from 11-link-css-js.html
 
-const button = document.getElementById('btn');
-const message = document.getElementById('message');
+const button = document.getElementsByTagName("button")[0];
+const message = document.getElementById("message");
 
-button.addEventListener('click', function () {
-  message.textContent = 'JavaScript changed this text!';
+button.addEventListener("click", function () {
+  message.textContent = "JavaScript changed this text!";
 });
